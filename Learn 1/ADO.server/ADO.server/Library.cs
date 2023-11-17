3e94f02509adddb80f91aca899ce1c1f87930a41
kuma <vityayuzbekov602@gmail.com>
2023-11-17 15:28:16 +0200

    lab 1

---
+++ Learn 1/ADO.server/ADO.server/Library.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ADO.server
+{
+    class library
+    {
+        string surname { get; set; } // фамилия посетителя
+        string name { get; set; } // имя посетителя
+        string book { get; set; } // название книги
+        string status { get; set; } // должник или нет
+        string date { get; set; } // дата выдачи книги
+        string author { get; set; } // автор книги
+    }
+}
---
+++ Learn 1/ADO.server/ADO.server/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Configuration; // for ConfigurationManager
+
+namespace ADO.server
+{
+    internal class Program
+    {
+
+        public static string connStr;
+        static void Main(string[] args)
+        {
+            connStr = ConfigurationManager.ConnectionStrings["kuma"].ConnectionString;
+        }
+    }
+}
