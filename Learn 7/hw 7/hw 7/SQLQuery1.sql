77a7a7c42887ce770ed480dff03e3ebdb808a9b7
kuma <vityayuzbekov602@gmail.com>
2023-12-03 13:16:42 +0200

    database from hw 7

---
+++ Learn 7/hw 7/hw 7/SQLQuery1.sql
@@ -0,0 +1,35 @@
+use Shop
+drop table Product
+drop table Sellers
+drop table Buyer
+drop table Sales
+
+create table Products (
+Id int not null identity primary key,
+[Name] nvarchar(max) not null check([Name]!=''),
+Model nvarchar(max) not null check(Model!=''),
+Price money not null default(0.01) check(Price!=''),
+[Count] int not null
+)
+
+create table Sellers (
+Id int not null identity primary key,
+FName nvarchar(100) not null check(FName!=''),
+Email nvarchar(100) null,
+Phone nvarchar(20) not null check(Phone!='')
+)
+
+create table Buyers (
+Id int not null identity primary key,
+FName nvarchar(100) not null check(FName!=''),
+Email nvarchar(100) not null check(Email!=''),
+Phone nvarchar(20) not null check(Phone!='')
+)
+
+create table Sales (
+Id int not null identity primary key,
+BuyerId int not null foreign key references Buyers(Id),
+SellerId int not null foreign key references Sellers(Id),
+ProductId int not null foreign key references Products(Id),
+SaleDate date not null
+)
